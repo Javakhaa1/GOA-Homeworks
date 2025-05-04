cba7358c4110085990068d40fc07864cd0bdb9f5
Javakhaa1 <javakhiamate@gmail.com>
2025-05-04 16:44:37 +0400

    Add files via upload

---
+++ Group 70/Day 3/Classwork.py
@@ -0,0 +1,11 @@
+name = "Alice"
+age = 25
+height = 5.4
+
+print("Name: " + name)
+print("Age:  + ag")
+print("Height: ", height)
+print("Hello " + name + "! Your age is " + age)
+
+number = 5 + 5
+print (number)
