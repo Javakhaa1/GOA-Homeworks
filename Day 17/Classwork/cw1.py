1945226acb0bd54a540a22b08272625ca2c3366b
Javakh1aa <javakhiamate@gmail.com>
2025-06-25 23:49:19 +0400

    commit

---
+++ Day 17/Classwork/cw1.py
@@ -0,0 +1,8 @@
+# 1)  შექმენით List, List-ში შეინახეთ 4 მანქანა და შემდეგ მე-1 და მე-2 ინდექსი შეცვალეთ, საბოლოოდ დაპრიტეთ მთლიანი List-ი
+
+list = ["Toyota", "Bmw", "Mercedes", "Bently"]
+
+list[1] = "Bmw"
+list[2] = "Mercedes"
+
+print (list)
